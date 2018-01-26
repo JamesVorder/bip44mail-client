e6460e2d31f0ffc1f07eb06c0ffbee41877c9ddd
JamesVorder <jamesvorder@gmail.com>
2018-01-26 17:47:10 -0600

    Added 'create keystore' and 'open keystore' functionality to the electron app.

--- examples/OpenKeystore.js
+++ examples/OpenKeystore.js
@@ -1,6 +1,6 @@
 var bc = require('../lib/BitterClient');
 
-bc.OpenKeystoreFromPath('keystore', function(err, data){
+bc.OpenKeystoreFromPath('/Users/jamesvorderbruggen/Documents/Source/bip44mail-client/examples/keystore', function(err, data){
     if(err) console.log(err);
     else{
         console.log(data);
--- main.js
+++ main.js
@@ -1,25 +1,57 @@
+//***************************
+//CONFIGURATION
+//***************************
+
+
 'use strict';
 
 var app = require('electron').app;
 var BrowserWindow = require('electron').BrowserWindow;
 var ipc = require('electron').ipcMain;
 var dialog = require('electron').dialog;
+var bc = require('./lib/BitterClient');
+var fs = require('fs');
 
 var mainWindow = null;
 
-app.on('ready', function() {
+var createWindow = function(){
     mainWindow = new BrowserWindow({
         frame: true,
-        resizable: false,
-        height: 500,
-        width: 500
+        resizable: true,
+        height: 800,
+        width: 1000
+    });
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        mainWindow = null;
     });
 
     mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+};
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function(){
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') app.quit();
 });
 
+app.on('activate', function(){
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) createWindow();
+});
+
+
+//<editor-fold desc='Application Code'>
 ipc.on('create-keystore-dialog', function(event){
     mainWindow.loadURL('FILE://' + __dirname + '/app/create-keystore-dialog.html');
+    mainWindow.webContents.on('did-finish-load', function(){
+        mainWindow.webContents.send('generate12Words', bc.generateNew12Words());
+    });
 });
 
 ipc.on('open-keystore-dialog', function(event){
@@ -30,6 +62,36 @@ ipc.on('open-file-dialog', function(event){
     dialog.showOpenDialog(mainWindow, {
         properties: ['openFile']
     }, function (files) {
-        if (files) event.sender.send('selected-file', files);
+        if (files) event.sender.send('selected-file', files[0]);
     });
 });
+
+ipc.on('OpenKeystoreFromPath', function(event, path){
+    bc.OpenKeystoreFromPath(path, function(err, data){
+       if(err) event.sender.send('OpenKeystoreFromPath-error', err);
+       else event.sender.send("OpenKeystoreFromPath-success", data);
+    });
+});
+
+ipc.on('CreateKeystore', function(event, pass){
+    bc.CreateKeystore(bc.generateNew12Words(), pass, function(err, data){
+        if(err) {
+            event.sender.send('CreateKeystore-error', err);
+        }
+        else {
+            dialog.showSaveDialog({ filters: [
+                    { name: 'bip44Mail-Vault', extensions: ['vault'] }
+                ]}, function (fileName) {
+
+                if (fileName === undefined) return;
+
+                fs.writeFile(fileName, data.serialize(), function (err) {
+
+                });
+
+            });
+        }
+        event.sender.send('CreateKeystore-success', data);
+    });
+});
+//</editor-fold>
