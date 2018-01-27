f4d4d139ce7465fa37adbf1a866bc6df8f356f02
Kopen <akopen@LAMU02RH4DLG8WP.uhc.com>
2018-01-26 18:02:12 -0600

    reformatted new changes again

--- examples/CreateKeystore.js
+++ examples/CreateKeystore.js
@@ -7,8 +7,7 @@ bc.CreateKeystore(bc.generateNew12Words(), "toor", function (err, ks) {
     bc.ExportKeystoreToPath('keystore', ks, function (err, data) {
       if (err) {
         console.log(err)
-      }
-      else {
+      } else {
         console.log('Success: ' + data)
       }
     });
