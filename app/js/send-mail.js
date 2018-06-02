f5888457f3d0ee252c97cbda459adcbe30faffd4
JamesVorder <jamesvorder@gmail.com>
2018-06-02 15:11:38 -0500

    Added drop-down "from" field.

--- app/js/send-mail.js
+++ app/js/send-mail.js
@@ -1,5 +1,32 @@
 var ipc = require('electron').ipcRenderer;
 
+window.onload = function(){
+  ipc.send('GetAddresses');
+}
+
+ipc.on('RefreshAddressList', function(event, addresses){
+  var list = "";
+  for(var addr in addresses) {
+    list += "<li class='address_option'>" + addresses[addr] + "</li>";
+  }
+  document.getElementById("menu").innerHTML = list;
+  var addressList = document.querySelectorAll('.address_option');
+  for(var i = 0; i < addressList.length; i++) {
+    console.log(addressList[i]);
+    addressList[i].addEventListener("click", function(){
+      document.getElementById("from").value = this.innerText;
+    }, false);
+  }
+});
+
+var showMenu = function(){
+  document.getElementById("menu").style.display="block";
+}
+
+var hideMenu = function(){
+  document.getElementById("menu").style.display="none";
+}
+
 var sendMail = function(){
   var to = document.getElementById('to').value;
   var from = document.getElementById('from').value;
